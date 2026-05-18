e903241c8f9a0ef499baac47dda5b6ec58d25787
BohdanIvanyna <ivanyna.bohdan@student.uzhnu.edu.ua>
2026-05-19 01:35:02 +0300

    Init frontend app and build Header

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div className="bg-gray-100">
+        <Header />
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="container mx-auto mt-5 rounded-xl py-2 px-4 w-4xl bg-white">
+      <h1>hello </h1>
+      <h1>hellod </h1>
+      <h1>hellod </h1>
+    </div>
+  );
+}
+
+export default Home;
